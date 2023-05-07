5b62c38903c584d0d2afb38a319cd51e6e4c5c2f
Amj14 <132676917+Amj14@users.noreply.github.com>
2023-05-07 21:15:44 +0800

    Add files via upload
    Signed-off-by: Amj14 <132676917+Amj14@users.noreply.github.com>

---
+++ SampleProject/dbo.Table_Company.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Table_Product]
+(
+	[ProductID] BIGINT NOT NULL PRIMARY KEY, 
+    [DriverID] BIGINT NULL, 
+    [Name of Product] VARCHAR(50) NULL, 
+    [Quantity] NCHAR(10) NULL, 
+    [Size] NCHAR(10) NULL, 
+    [Weight] NCHAR(10) NULL
+	FOREIGN KEY (DriverID) REFERENCES [dbo].[Table_Driver] (DriverID), 
+	FOREIGN KEY (CompanyID) REFERENCES [dbo].[Table_Company] (CompanyID),
+    [CompanyID] BIGINT NULL,
+)

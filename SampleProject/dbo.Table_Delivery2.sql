701b77cf9b8c3243b599331441cb2244d76f75a5
Joshua Victoriano <joshuavictoriano12@gmail.com>
2023-05-13 23:44:07 +0800

    SampleProject Deleted

--- SampleProject/dbo.Table_Delivery2.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Table_Company] (
-    [CompanyID]       BIGINT       NOT NULL,
-    [Name of Company] VARCHAR (50) NULL,
-    [DriverID]        BIGINT       NULL,
-    [Name of Driver]      VARCHAR(50)       NULL,
-	FOREIGN KEY (DriverID) REFERENCES [dbo].[Table](DriverID),
-    PRIMARY KEY CLUSTERED ([CompanyID] ASC)
-);
-

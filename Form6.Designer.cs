273de2c2dc21fe2567e65e3c42475a21dad6fc48
Joshua Victoriano <joshuavictoriano12@gmail.com>
2023-05-14 20:32:40 +0800

    Form 4 to 6

--- Form6.Designer.cs
+++ Form6.Designer.cs
@@ -53,12 +53,14 @@
             textBox2 = new TextBox();
             textBox1 = new TextBox();
             listView1 = new ListView();
+            textBox8 = new TextBox();
+            label11 = new Label();
             SuspendLayout();
             // 
             // dateTimePicker1
             // 
             dateTimePicker1.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            dateTimePicker1.Location = new Point(297, 522);
+            dateTimePicker1.Location = new Point(37, 520);
             dateTimePicker1.Name = "dateTimePicker1";
             dateTimePicker1.Size = new Size(239, 26);
             dateTimePicker1.TabIndex = 30;
@@ -66,7 +68,7 @@
             // comboBox2
             // 
             comboBox2.FormattingEnabled = true;
-            comboBox2.Location = new Point(563, 517);
+            comboBox2.Location = new Point(684, 504);
             comboBox2.Name = "comboBox2";
             comboBox2.Size = new Size(137, 23);
             comboBox2.TabIndex = 29;
@@ -74,14 +76,14 @@
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(563, 460);
+            comboBox1.Location = new Point(684, 447);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(137, 23);
             comboBox1.TabIndex = 28;
             // 
             // button4
             // 
-            button4.Location = new Point(607, 587);
+            button4.Location = new Point(598, 569);
             button4.Name = "button4";
             button4.Size = new Size(113, 23);
             button4.TabIndex = 27;
@@ -90,7 +92,7 @@
             // 
             // button3
             // 
-            button3.Location = new Point(461, 587);
+            button3.Location = new Point(466, 569);
             button3.Name = "button3";
             button3.Size = new Size(75, 23);
             button3.TabIndex = 26;
@@ -99,16 +101,16 @@
             // 
             // button2
             // 
-            button2.Location = new Point(297, 587);
+            button2.Location = new Point(307, 569);
             button2.Name = "button2";
-            button2.Size = new Size(75, 23);
+            button2.Size = new Size(84, 23);
             button2.TabIndex = 25;
             button2.Text = "UPDATE";
             button2.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            button1.Location = new Point(134, 587);
+            button1.Location = new Point(166, 569);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 24;
@@ -118,34 +120,34 @@
             // label10
             // 
             label10.AutoSize = true;
-            label10.Location = new Point(297, 499);
+            label10.Location = new Point(37, 497);
             label10.Name = "label10";
-            label10.Size = new Size(38, 15);
+            label10.Size = new Size(83, 15);
             label10.TabIndex = 22;
-            label10.Text = "label1";
+            label10.Text = "Date and Time";
             // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(563, 442);
+            label8.Location = new Point(684, 429);
             label8.Name = "label8";
-            label8.Size = new Size(38, 15);
+            label8.Size = new Size(27, 15);
             label8.TabIndex = 21;
-            label8.Text = "label1";
+            label8.Text = "Size";
             // 
             // label9
             // 
             label9.AutoSize = true;
-            label9.Location = new Point(563, 499);
+            label9.Location = new Point(684, 486);
             label9.Name = "label9";
-            label9.Size = new Size(38, 15);
+            label9.Size = new Size(45, 15);
             label9.TabIndex = 20;
-            label9.Text = "label1";
+            label9.Text = "Weight";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(803, 372);
+            label6.Location = new Point(869, 372);
             label6.Name = "label6";
             label6.Size = new Size(55, 15);
             label6.TabIndex = 19;
@@ -154,25 +156,25 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(57, 447);
+            label4.Location = new Point(277, 447);
             label4.Name = "label4";
-            label4.Size = new Size(100, 15);
+            label4.Size = new Size(88, 15);
             label4.TabIndex = 23;
-            label4.Text = "Name of Receiver";
+            label4.Text = "Contact Person";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(57, 499);
+            label2.Location = new Point(37, 447);
             label2.Name = "label2";
-            label2.Size = new Size(108, 15);
+            label2.Size = new Size(142, 15);
             label2.TabIndex = 18;
-            label2.Text = "Name of Company";
+            label2.Text = "Name of Client Company";
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(563, 390);
+            label7.Location = new Point(684, 384);
             label7.Name = "label7";
             label7.Size = new Size(53, 15);
             label7.TabIndex = 17;
@@ -181,7 +183,7 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(297, 442);
+            label5.Location = new Point(411, 393);
             label5.Name = "label5";
             label5.Size = new Size(86, 15);
             label5.TabIndex = 16;
@@ -190,7 +192,7 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(297, 390);
+            label3.Location = new Point(247, 393);
             label3.Name = "label3";
             label3.Size = new Size(55, 15);
             label3.TabIndex = 15;
@@ -199,7 +201,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(57, 390);
+            label1.Location = new Point(37, 393);
             label1.Name = "label1";
             label1.Size = new Size(87, 15);
             label1.TabIndex = 14;
@@ -207,7 +209,7 @@
             // 
             // textBox6
             // 
-            textBox6.Location = new Point(803, 390);
+            textBox6.Location = new Point(869, 390);
             textBox6.Multiline = true;
             textBox6.Name = "textBox6";
             textBox6.Size = new Size(264, 202);
@@ -215,15 +217,15 @@
             // 
             // textBox7
             // 
-            textBox7.Location = new Point(563, 408);
+            textBox7.Location = new Point(684, 402);
             textBox7.Multiline = true;
             textBox7.Name = "textBox7";
-            textBox7.Size = new Size(119, 31);
+            textBox7.Size = new Size(137, 24);
             textBox7.TabIndex = 11;
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(297, 460);
+            textBox5.Location = new Point(411, 411);
             textBox5.Multiline = true;
             textBox5.Name = "textBox5";
             textBox5.Size = new Size(158, 31);
@@ -231,7 +233,7 @@
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(57, 465);
+            textBox4.Location = new Point(277, 465);
             textBox4.Multiline = true;
             textBox4.Name = "textBox4";
             textBox4.Size = new Size(204, 31);
@@ -239,7 +241,7 @@
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(297, 408);
+            textBox3.Location = new Point(247, 411);
             textBox3.Multiline = true;
             textBox3.Name = "textBox3";
             textBox3.Size = new Size(158, 31);
@@ -247,7 +249,7 @@
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(57, 517);
+            textBox2.Location = new Point(37, 465);
             textBox2.Multiline = true;
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(234, 31);
@@ -255,7 +257,7 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(57, 408);
+            textBox1.Location = new Point(37, 411);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(204, 31);
@@ -268,6 +270,23 @@
             listView1.Size = new Size(1114, 316);
             listView1.TabIndex = 6;
             listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(487, 465);
+            textBox8.Multiline = true;
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(158, 31);
+            textBox8.TabIndex = 10;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(487, 447);
+            label11.Name = "label11";
+            label11.Size = new Size(86, 15);
+            label11.TabIndex = 16;
+            label11.Text = "Phone number";
             // 
             // Form6
             // 
@@ -288,11 +307,13 @@
             Controls.Add(label4);
             Controls.Add(label2);
             Controls.Add(label7);
+            Controls.Add(label11);
             Controls.Add(label5);
             Controls.Add(label3);
             Controls.Add(label1);
             Controls.Add(textBox6);
             Controls.Add(textBox7);
+            Controls.Add(textBox8);
             Controls.Add(textBox5);
             Controls.Add(textBox4);
             Controls.Add(textBox3);
@@ -301,6 +322,7 @@
             Controls.Add(listView1);
             Name = "Form6";
             Text = "Form6";
+            Load += Form6_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -332,5 +354,7 @@
         private TextBox textBox2;
         private TextBox textBox1;
         private ListView listView1;
+        private TextBox textBox8;
+        private Label label11;
     }
 }
